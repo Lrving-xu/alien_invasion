7db994e88023d00220bb89ab0cf0ef0428712022
Lrving.xu <lrving@htek.com>
2022-01-10 14:09:03 +0800

    完成了射击功能

--- alien_invasion.py
+++ alien_invasion.py
@@ -7,6 +7,7 @@ from settings import Settings
 from ship import Ship
 import game_functions as gf
 from pygame.sprite import Group
+from alien import Alien
 
 
 def run_game():
@@ -22,12 +23,17 @@ def run_game():
     # 创建一个用于存储子弹的编组
     bullets = Group()
 
+    # 创建一个外星人编组
+    aliens = Group()
+    gf.create_fleet(ai_settings, screen, ship, aliens)
+
     # 开始游戏的主循环
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+        gf.update_aliens(ai_settings, ship, aliens)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 
 run_game()
